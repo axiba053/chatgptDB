04db871bfa03b587854913c58805355d405e4d89
axiba053 <39181057+axiba053@users.noreply.github.com>
2023-11-16 08:38:34 +0800

    云服务器

--- pages/0_🎁_福利.py
+++ pages/0_🎁_福利.py
@@ -9,13 +9,16 @@ from PIL import Image
 
 airport="✈️  [魔法上网机场推荐](https://github.com/axiba053/ChatGPT-airport-tizi-fanqiang)"
 website=":flag-cn:  [ChatGPT国内镜像网站推荐](https://github.com/axiba053/awesome-free-chatgpt/tree/main)"
+txCloud=":partly_sunny: [云服务器推荐](https://curl.qcloud.com/AlTN7fIG)"
 
 
 image = Image.open('./image/AIgirl.png')
 st.set_page_config(page_title='三强的小屋', layout='wide', page_icon=image)
 
 st.markdown(website)
+st.markdown(txCloud)
 st.markdown(airport)
+
 
 sub = st.button('ChatGPT官方账号免费分享及其他福利')
 if sub:
